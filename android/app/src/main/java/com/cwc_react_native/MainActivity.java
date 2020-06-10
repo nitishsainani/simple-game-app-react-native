c9637fa53e934110f19c5e3fdee20cd4577420fe
Nitish Sainani <nitish.sainani@gmail.com>
2020-06-11 00:04:57 +0530

    changes

--- android/app/src/main/java/com/cwc_react_native/MainActivity.java
+++ android/app/src/main/java/com/cwc_react_native/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "CwC_React_Native";
+        return "Hello";
     }
 }
